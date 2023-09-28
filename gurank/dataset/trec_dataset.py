b65a662464b5697afaa7aef38f9bf43abbca1a6c
Hangsong Gu <guhangsong@outlook.com>
2023-09-28 04:00:26 +0000

    add datacollator test

--- gurank/dataset/trec_dataset.py
+++ gurank/dataset/trec_dataset.py
@@ -1,7 +1,18 @@
 from torch.utils.data import Dataset
+from torch import Tensor
+import torch
+from transformers.tokenization_utils_base import PreTrainedTokenizerBase
+from dataclasses import dataclass
 import ir_datasets
 from abc import ABC, abstractmethod
-from typing import Optional
+from typing import Optional, Union, Tuple, Dict, List, Any
+
+def create_sample(query: str, doc: str, prompt: Optional[str]) -> Union[str, Tuple]:
+    """Create sample by prompt"""
+    if prompt:
+        return prompt.format(query, doc)
+    
+    return [query, doc]
 
 class TrecDatasetBase(Dataset, ABC):
     """
@@ -34,11 +45,11 @@ class TrecDatasetBase(Dataset, ABC):
         self._qrels_dict = self._qrels.asdict()
         
     @abstractmethod
-    def __len__(self):
+    def __len__(self) -> int:
         pass
     
     @abstractmethod
-    def __getitem__(self, idx: int):
+    def __getitem__(self, idx: int) -> Dict[str, Any]:
         pass
 
     @property
@@ -61,10 +72,10 @@ class TrecDataset(TrecDatasetBase):
         super().__init__(name, doc_path, query_path, qrel_path)
         self._ids = dict(enumerate(self._qrels))
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self._ids)
 
-    def __getitem__(self, idx: int):
+    def __getitem__(self, idx: int) -> Dict[str, Union[str, int]]:
         qrel = self._ids[idx]
         rel = qrel.relevance
         qid = qrel.query_id
@@ -75,7 +86,7 @@ class TrecDataset(TrecDatasetBase):
         return {"idx": idx, "query": query, "doc": doc, "label": rel}
 
     @property
-    def ids_mapping(self):
+    def ids_mapping(self) -> List[Tuple[int, str, str]]:
         """Get mapping query_id and doc_id of idx"""
         return [(k, v.query_id, v.doc_id) for k, v in self._ids.items()]
 
@@ -93,10 +104,10 @@ class QueryTrecDataset(TrecDatasetBase):
         super().__init__(name, doc_path, query_path, qrel_path)
         self._ids = dict(enumerate(self._qrels_dict.keys()))
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self._ids)
     
-    def __getitem__(self, idx:int):
+    def __getitem__(self, idx:int) -> Dict[str, Union[str, List[Union[str,int]]]]:
         qid = self._ids[idx]
         query = self._dataset.queries.lookup(qid).text
         rels = self._qrels_dict[qid]
@@ -107,3 +118,67 @@ class QueryTrecDataset(TrecDatasetBase):
             labels.append(label)
         
         return {"query": query, "docs": docs, "labels": labels}
+
+
+@dataclass
+class DataCollator:
+    """
+    DataCollator for TrecDataset
+    """
+    tokenizer: PreTrainedTokenizerBase
+    max_length: Optional[int] = None
+    padding: Optional[bool] = True
+    truncation: Optional[Union[str, bool]] = "only_second"
+    prompt: Optional[str] = None
+    label_text: Optional[Tuple[str]] = None
+
+    def __call__(self, features: List[Dict[str, Union[str, int]]]) -> Dict[str, Tensor]:
+        idxs = torch.LongTensor([f["idx"] for f in features])
+        texts = [create_sample(f["query"], f["doc"], self.prompt) for f in features]
+        rels = [self.label_text[f["label"]] if self.label_text else f["label"] for f in features]
+        batch = self.tokenizer(
+            texts,
+            padding=self.padding,
+            truncation=self.truncation,
+            max_length=self.max_length,
+            return_tensors="pt"
+        )
+
+        labels = self.tokenizer(rels, return_tensors="pt").input_ids if self.label_text else torch.LongTensor(rels)
+        return idxs, batch, labels
+
+@dataclass
+class QueryDataCollator:
+    """
+    DataCollator for QueryDataset
+    """
+    tokenizer: PreTrainedTokenizerBase
+    docs_per_query: int
+    max_length: Optional[int] = None
+    padding: Optional[bool] = True
+    truncation: Optional[Union[str, bool]] = "only_second"
+    prompt: Optional[str] = None
+    label_text: Optional[Tuple[str]] = None
+
+    def __call__(self, features: List[Dict[str, Union[str, List[Union[str,int]]]]]) -> Dict[str, Tensor]:
+        texts = []
+        rels = []
+        for qds in features:
+            if not (len(qds["docs"] == qds("labels") == docs_per_query)):
+                raise ValueError(f"The docs or labels num under query {qds['query']} is not equal to docs_per_query")
+            for doc, label in zip(qds["docs"], qds["labels"]):
+                texts.append(create_sample(qds["query"], doc, self.prompt))
+                rels.append(self.label_text[label] if self.label_text else label)
+
+        batch = self.tokenizer(
+            texts,
+            padding=self.padding,
+            truncation=self.truncation,
+            max_length=self.max_length,
+            return_tensors="pt"
+        )
+
+        
+        labels = self.tokenizer(rels, return_tensors="pt").input_ids if self.label_text else torch.LongTensor(rels)
+
+        return batch, labels
--- tests/dataset/test_trec_dataset.py
+++ tests/dataset/test_trec_dataset.py
@@ -1,5 +1,6 @@
 import unittest
-from gurank.dataset.trec_dataset import TrecDataset, QueryTrecDataset
+from gurank.dataset.trec_dataset import TrecDataset, QueryTrecDataset, DataCollator, QueryDataCollator
+from transformers import AutoTokenizer
 
 class TrecDatasetTest(unittest.TestCase):
     def setUp(self):
@@ -70,6 +71,58 @@ class QueryTrecDatasetTest(unittest.TestCase):
             "query4": {"doc6": 1, "doc4": 0}
             }
         self.assertEqual(self._dataset.qrels_dict, expect_return)
+
+class DataCollatorTest(unittest.TestCase):
+    def setUp(self):
+        self.features =[
+            {"idx": 0,
+            "query": "What's the weather today?",
+            "doc": "It's raining now.",
+            "label": 1
+            },
+            {"idx": 1,
+            "query": "What's the weather today?",
+            "doc": "You really should read this interesting book.",
+            "label": 0
+            },
+            {"idx": 2,
+            "query": "Will future be good?",
+            "doc": "Tomorrow will be better.",
+            "label": 1
+            },
+            {"idx": 3,
+            "query": "Will future be good?",
+            "doc": "AI is rapidly changing the world.",
+            "label": 0
+            },]
+    def test_bert_collator(self):
+        model_path="tests/test_data/models/tiny-bert"
+        tokenizer = AutoTokenizer.from_pretrained(model_path)
+        collator = DataCollator(tokenizer=tokenizer)
+        idxs, batch, labels = collator(self.features)
+        self.assertEqual(idxs.tolist(), [0, 1, 2, 3])
+        self.assertEqual(labels.tolist(), [1, 0, 1, 0])
+        self.assertEqual(batch["input_ids"].tolist(), [
+            [101, 2054, 1005, 1055, 1996, 4633, 2651, 1029, 102, 2009, 1005, 1055, 24057, 2085, 1012, 102, 0, 0],
+            [101, 2054, 1005, 1055, 1996, 4633, 2651, 1029, 102, 2017, 2428, 2323, 3191, 2023, 5875, 2338, 1012, 102],
+            [101, 2097, 2925, 2022, 2204, 1029, 102, 4826, 2097, 2022, 2488, 1012, 102, 0, 0, 0, 0, 0],
+            [101, 2097, 2925, 2022, 2204, 1029, 102, 9932, 2003, 5901, 5278, 1996, 2088, 1012, 102, 0, 0, 0]])
+
+    def test_t5_collator(self):
+        model_path="tests/test_data/models/tiny-t5"
+        tokenizer = AutoTokenizer.from_pretrained(model_path)
+        prompt = "Query: {} Document: {} Relevant:"
+        label_text = ("false", "true")
+        collator = DataCollator(tokenizer=tokenizer, prompt=prompt, label_text=label_text)
+        idxs, batch, labels = collator(self.features)
+        self.assertEqual(idxs.tolist(), [0, 1, 2, 3])
+        self.assertEqual(labels.tolist(), [[1176, 1], [6136, 1], [1176, 1], [6136, 1]])
+        self.assertEqual(batch["input_ids"].tolist(), [
+            [3, 27569, 10, 363, 31, 7, 8, 1969, 469, 58, 11167, 10, 94, 31, 7, 3412, 53, 230, 5, 31484, 17, 10, 1, 0],
+            [3, 27569, 10, 363, 31, 7, 8, 1969, 469, 58, 11167, 10, 148, 310, 225, 608, 48, 1477, 484, 5, 31484, 17, 10, 1],
+            [3, 27569, 10, 2003, 647, 36, 207, 58, 11167, 10, 22365, 56, 36, 394, 5, 31484, 17, 10, 1, 0, 0, 0, 0, 0],
+            [3, 27569, 10, 2003, 647, 36, 207, 58, 11167, 10, 7833, 19, 7313, 2839, 8, 296, 5, 31484, 17, 10, 1, 0, 0, 0]])
+
 
 if __name__ == "__main__":
     unittest.main()
